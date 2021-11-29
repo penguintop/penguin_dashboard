480f23daba3b8e0cb0d0a2f4470b84d633a4cb14
luolong <52911940@qq.com>
2021-11-29 14:51:20 +0800

    fix stamp modal

--- src/pages/stamps/CreatePostageStampModal.tsx
+++ src/pages/stamps/CreatePostageStampModal.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useContext } from 'react'
+import React, { ReactElement, useContext, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
@@ -13,6 +13,9 @@ import { beeApi } from '../../services/bee'
 import { Context } from '../../providers/Stamps'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { useSnackbar } from 'notistack'
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown'
+import HelpIcon from '@material-ui/icons/Help'
+import axios from 'axios'
 
 interface FormValues {
   depth?: string
@@ -21,8 +24,8 @@ interface FormValues {
 }
 type FormErrors = Partial<FormValues>
 const initialFormValues: FormValues = {
-  depth: '',
-  amount: '',
+  depth: '22',
+  amount: '20',
   label: '',
 }
 
@@ -43,6 +46,47 @@ const useStyles = makeStyles((theme: Theme) =>
       marginTop: -12,
       marginBottom: -12,
     },
+    custom: {
+      border: '1px solid #e7e7e7',
+      borderRadius: '4px',
+      padding: '10px',
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+    },
+    customItem: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      fontSize: 16,
+      height: 24,
+    },
+    customItemInput: {
+      padding: theme.spacing(1),
+    },
+    balance: {
+      width: 552,
+      textAlign: 'right',
+    },
+    outline: {
+      lineHeight: '16px',
+    },
+    helpIcon: {
+      color: '#bdbdbd',
+      marginLeft: theme.spacing(1),
+    },
+    formula: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'right',
+      alignItems: 'center',
+      height: 24,
+    },
+    splitLine: {
+      background: '#e7e7e7',
+      height: '1px',
+      margin: '10px 0',
+    },
   }),
 )
 
@@ -57,6 +101,52 @@ export default function FormDialog({ label }: Props): ReactElement {
   const handleClickOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
   const { enqueueSnackbar } = useSnackbar()
+  const [custom, setCustom] = React.useState(false)
+  const handleCustom = () => setCustom(!custom)
+  const getPrice = (depth: any, amount: any, isValid: any) => {
+    if (!isValid || !depth || !amount) {
+      return ''
+    } else {
+      try {
+        const res = new BigNumber(2).pow(depth).times(amount).div(100000000).toFixed()
+        return res
+      } catch (e) {
+        return ''
+      }
+    }
+  }
+  const [myBalance, setMyBalance] = React.useState({
+    xwc: '0',
+    pen: '0',
+  })
+  useEffect(() => {
+    const getBalance = async () => {
+      const xwcRes = await axios({
+        url: `http://123.129.224.30:40123`,
+        method: 'post',
+        data: { "id": 1, "method": "get_addr_balances", "params": ["XWCNLveo8cXADqA62NNpC5Hvspx4pDbPoFL3p"] },
+      })
+      const penRes = await axios({
+        url: `http://123.129.224.30:40123`,
+        method: 'post',
+        data: { "id": 1, "method": "invoke_contract_offline", "params": ["defi", "XWCCbLjsxYhuJmdQiykz1AXHFcQuVwtNMWq5q", "balanceOf", "XWCNLveo8cXADqA62NNpC5Hvspx4pDbPoFL3p"] },
+      })
+      console.log(xwcRes.data.result, 111)
+      console.log(penRes.data.result, 222)
+      const res = {
+        xwc: '',
+        pen: '',
+      }
+      for (let i = 0; i < xwcRes.data.result.length; i++) {
+        if (xwcRes.data.result[i].asset_id === '1.3.0') {
+          res.xwc = new BigNumber(xwcRes.data.result[i].amount).div(100000000).toFixed()
+        }
+      }
+      res.pen = new BigNumber(penRes.data.result).div(100000000).toFixed()
+      setMyBalance(res)
+    }
+    getBalance()
+  }, [])
 
   return (
     <Formik
@@ -101,7 +191,7 @@ export default function FormDialog({ label }: Props): ReactElement {
         }
 
         // Label
-        if (values.label && !/^[0-9a-z]*$/i.test(values.label)) errors.label = 'Label must be an alphanumeric string'
+        // if (values.label && !/^[0-9a-z]*$/i.test(values.label)) errors.label = 'Label must be an alphanumeric string'
 
         return errors
       }}
@@ -115,20 +205,45 @@ export default function FormDialog({ label }: Props): ReactElement {
           <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
             <DialogTitle id="form-dialog-title">Purchase new postage stamp</DialogTitle>
             <DialogContent>
-              <DialogContentText>
+              {/* <DialogContentText>
                 Provide the depth, amount and optionally the label of the postage stamp. Please refer to the official pen docs to understand these values.
+              </DialogContentText> */}
+              <DialogContentText>
+                <div className={classes.balance}>{`Balance: ${myBalance.pen}PEN / ${myBalance.xwc}XWC`}</div>
               </DialogContentText>
-              <Field
-                component={TextField}
-                required
-                name="depth"
-                autoFocus
-                label="Depth"
-                fullWidth
-                className={classes.field}
-              />
-              <Field component={TextField} required name="amount" label="Amount" fullWidth className={classes.field} />
-              <Field component={TextField} name="label" label="Label" fullWidth className={classes.field} />
+              <div className={classes.custom}>
+                <div className={classes.customItem}>
+                  <div>Price</div>
+                  <div>{`${getPrice(values.depth, values.amount, isValid)} PEN`}</div>
+                </div>
+                <div className={classes.splitLine} />
+                <div className={classes.formula}>
+                  <div style={{ fontFamily: 'initial' }}>{`2^Depth(${values.depth})*Amount(${values.amount})/10^8`}</div>
+                  <a className={classes.outline} target="_blank" href="https://license.whitecoin.info/content/detail/61a07bf456f03452ab616058?lang=en">
+                    <HelpIcon className={classes.helpIcon} style={{ fontSize: "20px" }} />
+                  </a>
+                </div>
+              </div>
+              <div className={classes.custom}>
+                <div className={classes.customItem}>
+                  <div>Fee</div>
+                  <div>0.04XWC</div>
+                </div>
+              </div>
+              <div className={classes.custom}>
+                <div className={classes.customItem} onClick={handleCustom}>
+                  <div>Custom</div>
+                  <KeyboardArrowDown />
+                </div>
+                {
+                  custom &&
+                  <div className={classes.customItemInput}>
+                    <Field component={TextField} required name="depth" label="Depth" fullWidth className={classes.field} />
+                    <Field component={TextField} required name="amount" label="Amount" fullWidth className={classes.field} />
+                  </div>
+                }
+              </div>
+              {/* <Field component={TextField} name="label" label="Label" fullWidth className={classes.field} /> */}
             </DialogContent>
             <DialogActions>
               <Button onClick={handleClose} color="primary">
